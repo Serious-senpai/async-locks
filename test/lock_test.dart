3f0331aca1a1c13e9aa4dce62f6aee06ccfbc281
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2022-02-25 23:20:33 +0700

    Updated tests and examples

--- test/lock_test.dart
+++ test/lock_test.dart
@@ -2,24 +2,30 @@ import "package:locks/locks.dart";
 
 class Program {
   final lock = Lock();
+  final data = <int>[];
 
   Future<void> runFuture(int n) async {
-    print("Starting Future-$n");
     await lock.acquire();
-    print("Future-$n acquired the lock");
-    await Future.delayed(const Duration(seconds: 2));
-    print("Future-$n has slept for 2 seconds");
+    for (int i = n * 100; i < n + 100; i++) {
+      data.add(i);
+    }
     lock.release();
   }
 
   Future<void> run() async {
-    print("Running program");
     await Future.wait([runFuture(1), runFuture(2), runFuture(3), runFuture(4)]);
-    print("Finished!");
   }
+}
+
+bool incrementList(List<int> data) {
+  for (int i = 0; i < data.length - 2; i++) {
+    if (data[i] > data[i + 1]) return false;
+  }
+  return true;
 }
 
 void main() async {
   var program = Program();
   await program.run();
+  print(incrementList(program.data) ? "Test succeeded" : "Test failed");
 }
