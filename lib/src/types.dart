9e0afd40478728c71354a9a36470b573d68de65c
Haruka3399 <57554044+Saratoga-CV6@users.noreply.github.com>
2023-02-03 08:38:46 +0700

    Added documentation for exceptions

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -2,10 +2,14 @@ part of async_locks;
 
 typedef _FutureWaiter = Completer<void>;
 
+/// Base class for all exceptions from this package
 class AsyncLocksException implements Exception {}
 
+/// Exception thrown to futures cancelled by [Event.cancelAll]
 class EventCancelledException extends AsyncLocksException {}
 
+/// Exception thrown to futures cancelled by [Lock.cancelAll]
 class LockAcquireFailureException extends AsyncLocksException {}
 
+/// Exception thrown to futures cancelled by [Semaphore.cancelAll]
 class SemaphoreAcquireFailureException extends AsyncLocksException {}
