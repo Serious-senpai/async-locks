041ba4c766acd52c46610d2b97e658a46216ac59
Serious-senpai <57554044+Serious-senpai@users.noreply.github.com>
2024-09-14 09:06:59 +0700

    Remove duplicate documentation

--- lib/src/lock.dart
+++ lib/src/lock.dart
@@ -27,33 +27,6 @@ part of "../async_locks.dart";
 /// ```
 ///
 /// See also: [Python documentation](https://docs.python.org/3/library/asyncio-sync.html#asyncio.Lock)
-/// Mutex lock to guarantee exclusive access to a shared state.
-///
-/// A [Lock] object can be in one of two states: "locked" or "unlocked".
-///
-/// If the lock is "locked", all futures that call [acquire] will be put in a waiting FIFO queue
-/// and will proceed in order for each [release] call.
-///
-/// If the lock is "unlocked", calling [acquire] will set the lock to the "locked" state and
-/// return immediately.
-///
-/// Example usage:
-/// ```dart
-/// final lock = Lock();
-///
-/// // Acquire the lock
-/// await lock.acquire();
-///
-/// try {
-///   // Perform exclusive operations on the shared state
-///   // ...
-/// } finally {
-///   // Release the lock
-///   lock.release();
-/// }
-/// ```
-///
-/// See also: [Python documentation](https://docs.python.org/3/library/asyncio-sync.html#asyncio.Lock)
 class Lock extends _Acquirable {
   final _waiters = ListQueue<_FutureWaiter>();
   bool _locked = false;
